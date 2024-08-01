fb77efaae5278f013feca63fdd16c1d688824b16
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-08-02 00:53:38 +0530

    Update AudioInterruption.m
    Final changes

--- src/ios/AudioInterruption.m
+++ src/ios/AudioInterruption.m
@@ -25,21 +25,22 @@
 {
     if ([notification.name isEqualToString:AVAudioSessionInterruptionNotification]) {
         // Check to see if it was a Begin interruption
-        NSString *interruptionType;
         NSString *statusMessage;
 
         if ([[notification.userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeBegan]]) {
             CTCallCenter *callCenter = [[CTCallCenter alloc] init];
             CTCall *call = [[callCenter currentCalls] anyObject];
             if (call != nil && call.callState == CTCallStateIncoming) {
-                interruptionType = @"INCOMING_CALL";
+                statusMessage = @"INCOMING_CALL";
             }
-          statusMessage = [NSString stringWithFormat:@"INTERRUPTION_BEGIN_%@", interruptionType];
-            [self sendStatusNameInJS:statusMessage];
+            else{
+                statusMessage =@"INTERRUPTION_BEGIN";
+            }
+          
         } else {
-           statusMessage = [NSString stringWithFormat:@"INTERRUPTION_ENDED_%@", interruptionType];
-           [self sendStatusNameInJS:statusMessage];
+           statusMessage = @"INTERRUPTION_ENDED"; 
         }
+        [self sendStatusNameInJS:statusMessage];
     }
 }
 
